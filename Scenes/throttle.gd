c6596253a89bd208f2a279aadd7fe2a3b959d0a7
Chase Odom <chasemichaelodom@gmail.com>
2024-06-02 09:45:31 -0700

    animated throttle
    👍

--- Scenes/throttle.gd
+++ Scenes/throttle.gd
@@ -1,5 +1,8 @@
 extends Control
 
+const ANIM_TIME: float = 0.1
+
+var throttle_tweener : Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,9 +10,13 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func update(speed : int):
+	if throttle_tweener:
+		throttle_tweener.kill()
+	throttle_tweener = create_tween()
 	#$ThrottleKnob.position.y = 190 - (50 * speed)
-	$ThrottleKnob.position = $ThrottleKnob.position.lerp(Vector2(0, 190 - (50 * speed)), 1)
+	var new_knob_pos = $ThrottleKnob.position.lerp(Vector2(0, 190 - (50 * speed)), 1)
+	throttle_tweener.tween_property($ThrottleKnob, "position", new_knob_pos, ANIM_TIME).set_trans(Tween.TRANS_BOUNCE)
