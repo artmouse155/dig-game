37ed533f7825c6e48a2449f7ceac020df0410831
Chase Odom <chasemichaelodom@gmail.com>
2024-06-11 11:23:57 -0700

    crafting UI upgrade

---
+++ Scripts/Crafting UI/recipe_icon.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var special_fx: Node2D
+@export var sprite : Sprite2D
+
+signal clicked
+
+func show_glow(b: bool):
+	special_fx.visible = b
+
+func _input(event):
+	if event is InputEventMouseButton\
+	and event.button_index == MOUSE_BUTTON_LEFT\
+	and event.pressed\
+	and sprite.get_rect().has_point(get_local_mouse_position()):
+		pass
+		#clicked.emit()
--- Scripts/Crafting UI/recipe_panel.gd
+++ Scripts/Crafting UI/recipe_panel.gd
@@ -9,12 +9,16 @@ extends PanelContainer
 
 @export var scale_curve: Curve
 
+const RECIPE_SCENE = preload("res://Scenes/Crafting/recipe.tscn")
+
 const SCROLL_STRENGTH = 50
 const DRAG_STRENGTH = 1
 const RECIPE_SIZE = Vector2(150,150)
 const RECIPE_SEPARATION = 200
 
 const SCALE_EFFECT_RADIUS = 300
+
+const GLIDE_SPEED: float = 1000.0
 
 var current_recipe_index: int:
 	get:
@@ -30,6 +34,9 @@ var recipe_icons: Array[Node] = []
 var touching_mouse = false
 var dragging_with_mouse = false
 
+var glide_tween: Tween
+var glide_amt = 0.0
+
 func _ready():
 	init_with_recipes(range(5))
 	mouse_entered.connect(set_touching_mouse.bind(true))
@@ -42,16 +49,16 @@ func set_touching_mouse(b):
 
 func init_with_recipes(recipe_list):
 	for i in range(len(recipe_list)):
-		var temp_recipe_icon = ColorRect.new()
-		#
-		var random_color = Color(randf(), randf(), randf(), 1) # Generating random color
-		temp_recipe_icon.color = random_color
-		#temp_recipe_icon.modulate = random_color
+		var temp_recipe_icon = RECIPE_SCENE.instantiate()
+		temp_recipe_icon.clicked.connect(jump_to_recipe_index.bind(i))
+		temp_recipe_icon.show_glow(i == 2 or i == 3)
+		#var random_color = Color(randf(), randf(), randf(), 1) # Generating random color
+		#temp_recipe_icon.color = random_color
 		
-		temp_recipe_icon.mouse_filter = Control.MOUSE_FILTER_IGNORE
-		temp_recipe_icon.size = RECIPE_SIZE
-		temp_recipe_icon.pivot_offset = temp_recipe_icon.size * 0.5
-		temp_recipe_icon.position = temp_recipe_icon.size * -0.5
+		#temp_recipe_icon.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		#temp_recipe_icon.size = RECIPE_SIZE
+		#temp_recipe_icon.pivot_offset = temp_recipe_icon.size * 0.5
+		#temp_recipe_icon.position = temp_recipe_icon.size * -0.5
 		temp_recipe_icon.position.x += RECIPE_SEPARATION * i
 		
 		var pos_along: float = recipe_progress + (i * RECIPE_SEPARATION)
@@ -87,30 +94,45 @@ func _input(event):
 				dragging_with_mouse = false
 				current_recipe_index = -round(recipe_progress / RECIPE_SEPARATION)
 				jump_to_recipe_index(current_recipe_index)
-	
-			
-func scroll_recipe_progress(pixels):
-	var new_pos = clamp(recipe_progress + pixels, -RECIPE_SEPARATION * (len(recipe_icons) - 1), 0)
-	var change = new_pos - recipe_progress
-	recipe_progress = new_pos
-	for i in range(len(recipe_icons)):
-		var node = recipe_icons[i]
-		node.position.x += change
-		var pos_along: float = recipe_progress + (i * RECIPE_SEPARATION)
-		#print("pos_along:", pos_along)
-		if (pos_along < SCALE_EFFECT_RADIUS) and (pos_along > -SCALE_EFFECT_RADIUS):
-			var x_val = 1 - abs(pos_along / SCALE_EFFECT_RADIUS)
-			#print("x_val:", x_val)
-			node.scale = Vector2.ONE * scale_curve.sample(x_val)
-		else:
-			node.scale = Vector2.ONE
-	left_gear.rotation_degrees += change
-	right_gear.rotation_degrees += change
+
+func start_new_glide():
+	glide_amt = 0.0
+
+func scroll_recipe_progress_internal_glide(amt):
+	var change = amt - glide_amt
+	glide_amt = amt
+	scroll_recipe_progress(change)
+
+func scroll_recipe_progress(pixels, glide: bool = false):
+	if glide:
+		if glide_tween:
+			glide_tween.kill()
+		glide_tween = create_tween()
+		start_new_glide()
+		glide_tween.tween_method(scroll_recipe_progress_internal_glide, 0, pixels, abs(pixels / GLIDE_SPEED)).set_trans(Tween.TRANS_SINE)
+		glide_tween.tween_callback(glide_tween.kill)
+	else:
+		var new_pos = clamp(recipe_progress + pixels, -RECIPE_SEPARATION * (len(recipe_icons) - 1), 0)
+		var change = new_pos - recipe_progress
+		recipe_progress = new_pos
+		for i in range(len(recipe_icons)):
+			var node = recipe_icons[i]
+			node.position.x += change
+			var pos_along: float = recipe_progress + (i * RECIPE_SEPARATION)
+			#print("pos_along:", pos_along)
+			if (pos_along < SCALE_EFFECT_RADIUS) and (pos_along > -SCALE_EFFECT_RADIUS):
+				var x_val = 1 - abs(pos_along / SCALE_EFFECT_RADIUS)
+				#print("x_val:", x_val)
+				node.scale = Vector2.ONE * scale_curve.sample(x_val)
+			else:
+				node.scale = Vector2.ONE
+		left_gear.rotation_degrees += change
+		right_gear.rotation_degrees += change
 
 func jump_to_recipe_index(index):
 	var change =  -(index * RECIPE_SEPARATION) - recipe_progress
 	current_recipe_index = index
-	scroll_recipe_progress(change)
+	scroll_recipe_progress(change, true)
 
 func left_button_pressed():
 	if current_recipe_index > 0:
